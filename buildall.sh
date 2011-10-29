ccf8a41a00de9eb34d64958a853ac3c7432b9103
Troy Straszheim <straszheim@willowgarage.com>
2011-10-29 09:10:41 -0700

    more deb packaging

--- buildall.sh
+++ buildall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -ex
 
-TOP=$(cd dirname $0 ; pwd)
+TOP=$(cd `dirname $0` ; pwd)
 
 INSTDIR=$TOP/inst
 rm -rf $INSTDIR
@@ -11,24 +11,50 @@ rm -f *.build *.deb *.changes
 echo "INSTDIR=$INSTDIR"
 # INSTDIR=/tmp/ros/electric
 
-CMAKE="cmake -DCMAKE_INSTALL_PREFIX=$INSTDIR -DCMAKE_PREFIX_PATH=$TOP/cmake"
+#CMAKE="cmake -DCMAKE_INSTALL_PREFIX=$INSTDIR -DCMAKE_PREFIX_PATH=$TOP/cmake"
+CMAKE="cmake"
+PPA="ppa:straszheim/ros"
 
 rm -rf $TOP/build
 
-PACKAGES="ros_cmake foo bar snake simpleinstall"
-for l in $PACKAGES
+PACKAGES="rosbuild foo" #  simpleinstall" #bar snake simpleinstall quux_msgs quux_user"
+
+#
+#  Local test
+#
+cd $TOP
+for p in $PACKAGES
 do
-    mkdir -p $BUILDDIR/$l
-    cd $BUILDDIR/$l
-    $CMAKE $TOP/$l
-    make VERBOSE=1
-    make VERBOSE=1 install
-    
-    if [ -d $TOP/$l/debian ] ; then
-        cd $TOP/$l
-        debuild -i -uc -us -b
-    fi
+    echo $p
+    rm -f ${p}_?.?.?_*.deb
+    rm -f ${p}_?.?.?_*.upload
+    pushd $TOP/$p
+    dpkg-buildpackage -F
+    debuild -S
+    popd
+    sudo dpkg -i ${p}_?.?.?_*.deb
+    dput ppa:straszheim/ros $p*_source.changes
 done
+
+exit
+# for p in $PACKAGES
+# do
+#     echo $p
+#     cd $TOP
+#     rm $p_?.?.?.*
+#     cd $BUILDDIR/$p
+#     $CMAKE $TOP/$l
+#     make
+#     make install
+#
+#     if [ -d $TOP/$l/debian ] ; then
+#         pushd $TOP/$l
+#         debuild -S
+#         popd
+#         dput ppa:straszheim/ros $i*_source.changes
+#     fi
+# done
+
 
 # ls -ltR $INSTDIR
 
