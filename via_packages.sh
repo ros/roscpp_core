7b799b73b267b90ae68608ff29a0286d381400cd
Troy Straszheim <straszheim@willowgarage.com>
2011-10-30 12:30:42 -0700

    bing

--- via_packages.sh
+++ via_packages.sh
@@ -2,6 +2,11 @@
 
 TOP=$(cd `dirname $0` ; pwd)
 
+cd $TOP
+git clean -fdx
+$(cd $TOP/genmsg ; git clean -fdx)
+$(cd $TOP/gencpp ; git clean -fdx)
+
 INSTDIR=$TOP/inst
 rm -rf $INSTDIR
 BUILDDIR=$TOP/build
@@ -12,21 +17,34 @@ rm -f *.build *.deb *.changes
 CMAKE="cmake"
 PPA="ppa:straszheim/ros"
 
+mkdir -p PKGS
+export DEB_DESTDIR=$TOP/PKGS
+export CMAKE_INSTALL_PREFIX=$TOP/INSTALLY
+export DEB_BUILD_DIR_PFX=$TOP/DEBBUILD
+
 function dodir {
-    rm -f $1*.deb
+    rm -f ${2}_*.*.* /tmp/pkg/*
     cd $1
-    dpkg-buildpackage
     dh_clean -v
+    debuild \
+        -e DH_VERBOSE=1 \
+        -e DH_OPTIONS="-v -P$TOP/$1/pkg_tmpdir" \
+        -e DEB_SOURCE_DIR=`pwd` \
+        -e DEB_BUILD_DIR=$DEB_BUILD_DIR_PFX/$1 \
+        -e DEB_DEBS_DIR=$TOP/debs
+    #dpkg-buildpackage -tc # --changes-option=-u/tmp/pkg #--source-option=-VPackage=MOOKAH -v9.9.9
+    #dpkg-buildpackage
+    # dh_clean -v
     cd ..
     sudo dpkg -i $2*.deb
 }
 
 #dodir rosbuild rosbuild
 #dodir foo foo
-#dodir bar bar
+dodir bar bar
 #dodir genmsg ros-fuerte-genmsg
 #dodir gencpp ros-fuerte-gencpp
-dodir std_msgs std-msgs
+#dodir std_msgs ros-std-msgs
 
 exit
 
@@ -44,7 +62,7 @@ do
     rm -f ${p}_?.?.*_*.deb
     rm -f ${p}_?.?.*_*.upload
     pushd $TOP/$p
-    dpkg-buildpackage
+    dpkg-buildpackage -tc --source-option=-VPackage=MOOKAH -v9.9.9 # --changes-option=-u$(DEB_DESTDIR)
     popd
     sudo dpkg -i ${p}_?.?.*_*.deb
     dput ppa:straszheim/ros $p*_source.changes
