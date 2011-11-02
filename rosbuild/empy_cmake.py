45440071fa0890c3487be64f7f8c1a06c2de27ed
Morten Kjaergaard <mkjargaard@willowgarage.com>
2011-11-02 11:32:52 -0700

    Get it in there!!

--- rosbuild/empy_cmake.py
+++ rosbuild/empy_cmake.py
@@ -1,2 +1,13 @@
-def someutil(x):
-    return "SOME UTIL IS RUNNING <<" + str(x) + ">>"
+def aslist(x):
+    if x == "":
+        return []
+    else:
+        return x.split(';')
+
+def asdict(x):
+    dict = {}
+    for str in aslist(x):
+        pair = str.split(":")
+        dict[pair[0]] = pair[1]
+
+    return dict
