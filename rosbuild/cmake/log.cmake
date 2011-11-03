4cd33140db1d885d2cb430e66b8fc2687535f902
Morten Kjaergaard <mkjargaard@willowgarage.com>
2011-11-02 19:05:02 -0700

    error in cmake log function

--- rosbuild/cmake/log.cmake
+++ rosbuild/cmake/log.cmake
@@ -1,5 +1,15 @@
+# Log levels
+# 0 Normal use
+# 1 Rosbuild developer use (Stuff being developed)
+# 2 Rosbuild developer use (Stuff working)
+# 3 Also Print True Assert Statements
+
 function(LOG ARG_LEVEL ARG_MSG)
-  if(ARG_LEVEL > ROS_LOG_LEVEL)
+  if(NOT ROS_LOG_LEVEL)
+    set(ROS_LOG_LEVEL 0)
+  endif()
+
+  if(NOT ARG_LEVEL GREATER ${ROS_LOG_LEVEL})
     message(STATUS "*${ARG_LEVEL}* ${ARG_MSG}")
   endif()
 endfunction()
