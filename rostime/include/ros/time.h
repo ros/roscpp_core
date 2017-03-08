426fc3fcbab21e73505c66ebd4bd3030b8f95039
Felix Ruess <felix.ruess@roboception.de>
2017-03-08 16:28:39 +0100

    [rostime] add SteadyTime
    Add a new SteadyTime wich uses a steady/monotonic clock to prevent issues with time jumps.

--- rostime/include/ros/time.h
+++ rostime/include/ros/time.h
@@ -256,8 +256,43 @@ namespace ros
     static bool isSystemTime() { return true; }
   };
 
+  /**
+   * \brief Time representation.  Always steady-clock time.
+   *
+   * Not affected by ROS time.
+   *
+   * ros::TimeBase provides most of its functionality.
+   */
+  class ROSTIME_DECL SteadyTime : public TimeBase<SteadyTime, WallDuration>
+  {
+    public:
+      SteadyTime()
+        : TimeBase<SteadyTime, WallDuration>()
+      {}
+
+      SteadyTime(uint32_t _sec, uint32_t _nsec)
+        : TimeBase<SteadyTime, WallDuration>(_sec, _nsec)
+      {}
+
+      explicit SteadyTime(double t) { fromSec(t); }
+
+      /**
+       * \brief Returns the current steady (monotonic) clock time.
+       */
+      static SteadyTime now();
+
+      /**
+       * \brief Sleep until a specific time has been reached.
+       * @return True if the desired sleep time was met, false otherwise.
+       */
+      static bool sleepUntil(const SteadyTime& end);
+
+      static bool isSystemTime() { return true; }
+  };
+
   ROSTIME_DECL std::ostream &operator <<(std::ostream &os, const Time &rhs);
   ROSTIME_DECL std::ostream &operator <<(std::ostream &os, const WallTime &rhs);
+  ROSTIME_DECL std::ostream &operator <<(std::ostream &os, const SteadyTime &rhs);
 }
 
 #endif // ROS_TIME_H
